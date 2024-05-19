4d5322308fc7f6884cf9c3197190dc7bf0982612
[Ibukunoluwa Adekunle] <[ibkadekunle2004@gmail.com]>
2024-05-19 07:22:07 +0100

    local uploads

--- api/index.js
+++ api/index.js
@@ -2,197 +2,234 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
-const protectedRoutes = require('./routes/protectedRoutes')
-
-
-const multer = require('multer')
-const { GridFsStorage } = require("multer-gridfs-storage")
-var crypto = require('crypto');
-var path = require('path');
-
-
-//const multer = require('multer');
-//const { GridFsStorage } = require('multer-gridfs-storage');
-//const crypto = require('crypto');
-//const path = require('path');
-//const cors = require('cors');
+const protectedRoutes = require('./routes/protectedRoutes');
+const multer = require('multer');
+const { GridFsStorage } = require('multer-gridfs-storage');
+const crypto = require('crypto');
+const path = require('path');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const authMiddleware = require('./middlewares/authMiddleware');
 const User = require('./models/user');
 const Album = require('./models/album');
-
-
-const cors = require('cors');
-
-var livereload = require("livereload");
-var connectLiveReload = require("connect-livereload");
-
+const livereload = require('livereload');
+const connectLiveReload = require('connect-livereload');
+const fs = require('fs'); // Import fs module
 require('dotenv').config();
 
 const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
+liveReloadServer.server.once('connection', () => {
   setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
+    liveReloadServer.refresh('/');
+  }, 5000);
 });
 
 const app = express();
+app.use(express.static('public'));
 app.use(connectLiveReload());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
-
 app.use(cors({
-	origin: ['http://localhost:5173', 'https://resume-maker-cyan.vercel.app', 'https://album-maker.vercel.app'],
+  origin: ['http://localhost:5173', 'https://resume-maker-cyan.vercel.app', 'https://album-maker.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // db.js
-mongoose.connect((`${process.env.DATABASE_URI}`))
+mongoose.connect(process.env.DATABASE_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-
 let bucket;
-mongoose.connection.on("connected", () => {
-  var db = mongoose.connections[0].db;
+mongoose.connection.on('connected', () => {
+  const db = mongoose.connections[0].db;
   bucket = new mongoose.mongo.GridFSBucket(db, {
-    bucketName: "newBucket"
+    bucketName: 'uploads'
   });
   console.log(bucket);
-})
+});
 
-//to parse json content
 app.use(express.json());
-//to parse body from url
-app.use(express.urlencoded({
-  extended: false
-}))
+
+app.use(express.urlencoded({ extended: false }));
 
 const storage = new GridFsStorage({
   url: process.env.DATABASE_URI,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
     return new Promise((resolve, reject) => {
       const filename = file.originalname;
       const fileInfo = {
         filename: filename,
-        bucketName: "uploads"
+        bucketName: 'uploads'
       };
       resolve(fileInfo);
     });
   }
 });
 
-const upload = multer({
-  storage
+const upload = multer({ storage });
+
+const localStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    const filePath = path.join('public/images', file.originalname);
+    if (fs.existsSync(filePath)) {
+      cb(null, file.originalname); // Allow the file with the same name, we will handle this in the POST route
+    } else {
+      cb(null, file.originalname);
+    }
+  }
 });
 
+const localUpload = multer({ storage: localStorage });
 
-app.post("/upload/image", upload.single("file"), (req, res) => {
- 
-    res.status(201).json({
-      message: "File uploaded successfully",
-      status: "success",
-      ok: true,
-    });
-});
+app.post('/local/upload', localUpload.array('files', 70), async (req, res) => {
+  const authHeader = req.headers['authorization'];
 
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Authorization header is missing' });
+  }
 
-app.post('/upload/images', authMiddleware, upload.array('files', 120), async (req, res) => {
+  // Extract the token from the Authorization header
+  const token = authHeader.split(' ')[1];
 
-  const { data } = req.body;
-  const parsedData = JSON.parse(data); 
+  if (!token) {
+    return res.status(401).json({ error: 'Token is missing' });
+  }
 
-  const userId = parsedData.userId;
-  const albumName= parsedData.albumName;
+  console.log('Received /local/upload request');
+  console.log('Request body:', req.body);
+  console.log('Uploaded files:', req.files);
 
+  const { userId, albumName } = req.body;
+
+  if (!userId || !albumName) {
+    console.log('Missing userId or albumName');
+    return res.status(400).json({ error: 'albumName is required' });
+  }
 
   try {
-    // Find user by userId
-    const user = await User.findOne({ user_id: userId });
+    // Find the user by userId
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Extract user information from the decoded token
+    const userId = decoded.sub;
+
+    console.log('Searching for user with userId:', userId);
+    const user = await User.findOne({ _id: userId });
     if (!user) {
+      console.log('User not found');
       return res.status(404).json({ message: 'User not found' });
     }
 
-    // Check if album already exists or create a new one
-    let album = await Album.findOne({ userId: user._id, name: albumName });
-    if (!album) {
-      album = new Album({
-        userId: user._id,
-        name: albumName,
-        uploadedImages: [],
-        pages: [
-          {
-            page: 1,
-            mainPreviewImages: [],
-            editedImages: [],
-            positionsData: [{ x: 0, y: 0 }],
-            zoomData: [1],
-            template: 'template 1',
-            visible: true,
-          },
-        ],
-      });
-    }
-
-    // Add image URLs to the album's mainPreviewImages and editedImages arrays
-    files.forEach((file) => {
-      const imageUrl = `https://express-backend-9bou.onrender.com/api/files/${file.filename}`;
-      //album.pages[0].mainPreviewImages.push(imageUrl);
-      //album.pages[0].editedImages.push(imageUrl);
-      album.uploadedImages.push(imageUrl);
+    // Create a new album
+    console.log('Creating new album for user:', user._id);
+    const newAlbum = new Album({
+      userId: user._id,
+      name: albumName,
+      uploadedImages: [],
+      pages: [{
+        page: 1,
+        mainPreviewImages: [],
+        editedImages: [],
+        positionsData: [{ x: 0, y: 0 }],
+        zoomData: [1],
+        template: 'template 1',
+        visible: true,
+      }],
     });
 
-    // Save the updated album
-    await album.save();
+    // Store URLs of the uploaded images in the uploadedImages array
+    req.files.forEach((file) => {
+      const imageUrl = `https://express-backend-9bou.onrender.com/images/${file.filename}`;
+      newAlbum.uploadedImages.push(imageUrl);
+    });
 
-    // Add the album to the user's albums array if it's a new album
-    if (!user.albums.includes(album._id)) {
-      user.albums.push(album._id);
-      await user.save();
-    }
+    // Save the new album to the database
+    console.log('Saving new album to the database');
+    await newAlbum.save();
+
+    // Add the album to the user's albums array
+    console.log('Adding new album to user\'s albums');
+    user.albums.push(newAlbum._id);
+    await user.save();
 
     res.status(201).json({
       userId: userId,
       albumName: albumName,
-      albumId: album._id,
-      message: "File uploaded successfully",
+      message: 'Files uploaded successfully',
       files: req.files,
-      status: "success",
+      status: 'success',
       ok: true,
     });
-  
-  
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error uploading files', error });
+    console.error('Error occurred:', error);
+    res.status(500).json({ error: 'Something went wrong' });
   }
 });
 
-  
+app.get('/');
 
+app.post('/upload/image', upload.single('file'), (req, res) => {
+  res.status(201).json({
+    message: 'File uploaded successfully',
+    status: 'success',
+    ok: true,
+  });
+});
 
-// Routes
+app.post('/upload/images', authMiddleware, upload.array('files', 80), (req, res) => {
+  const { data } = req.body;
+  const parsedData = JSON.parse(data);
+  const userId = parsedData.userId;
+  const albumName = parsedData.albumName;
 
+  res.status(201).json({
+    userId: userId,
+    albumName: albumName,
+    message: 'File uploaded successfully',
+    files: req.files,
+    status: 'success',
+    ok: true,
+  });
+});
+
+app.get('/fileinfo/:filename', (req, res) => {
+  bucket.find({ filename: req.params.filename }).toArray((err, files) => {
+    if (!files[0] || files.length === 0) {
+      return res.status(404).json({ err: 'No files exist' });
+    }
+
+    if (err) {
+      return res.status(500).json({ err: err.message });
+    }
+
+    if (['image/jpeg', 'image/png', 'image/svg+xml'].includes(files[0].contentType)) {
+      bucket.openDownloadStreamByName(req.params.filename).pipe(res);
+    } else {
+      res.status(404).json({ err: 'Not an image' });
+    }
+  });
+});
 
 app.use(userRoutes);
 app.use(protectedRoutes);
 
-// Default route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Error handling middleware (placed at the end)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+server.setTimeout(600000);
