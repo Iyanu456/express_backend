344c6b6cf8c27dca765f6466357a074cc9323a83
Iyanu <oyerindei13@gmail.com>
2024-05-23 20:51:25 +0100

    Update index.js after reverting to local uploads

--- api/index.js
+++ api/index.js
@@ -198,6 +198,64 @@ app.post('/upload/images', authMiddleware, upload.array('files', 80), (req, res)
   });
 });
 
+app.get('/album/:albumid', async (req, res) => {
+    const albumId = req.params.albumid;
+  
+    try {
+      const album = await Album.findOne({ _id: albumId });
+  
+      if (!album) {
+        return res.status(404).json({ message: 'Album not found' });
+      }
+  
+      const uploadedImages = album.uploadedImages;
+  
+      res.status(200).json({
+        albumId: album._id,
+        message: 'Images retrieved successfully',
+        imageUrls: uploadedImages,
+        status: 'success',
+        ok: true,
+      });
+    } catch (error) {
+      console.error('Error retrieving album:', error);
+      res.status(500).json({ error: `${error}` });
+    }
+  });
+
+
+
+
+  app.get('/albums/:userId', async (req, res) => {
+    const { userId } = req.params;
+  
+    try {
+      // Find the user by userId and populate the albums
+      const user = await User.findById(userId).populate('albums', 'name');
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      // Map the albums to only include name and _id (albumId)
+      const albumList = user.albums.map(album => ({
+        albumId: album._id,
+        albumName: album.name
+      }));
+  
+      // Return the list of albums
+      res.status(200).json({
+        userId: userId,
+        albums: albumList,
+        status: 'success',
+        ok: true
+      });
+    } catch (error) {
+      console.error('Error occurred:', error);
+      res.status(500).json({ error: `${error}` });
+    }
+  });
+
 app.get('/fileinfo/:filename', (req, res) => {
   bucket.find({ filename: req.params.filename }).toArray((err, files) => {
     if (!files[0] || files.length === 0) {
