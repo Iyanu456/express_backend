2bbefed8644c0227ec7a7c037949fdedb952203f
Iyanu456 <oyerindei13@gmail.com>
2024-09-06 16:45:29 +0100

    implemnted secret key in header to secure webhook endpoint

--- api/routes/webhooks/paymentNotification.js
+++ api/routes/webhooks/paymentNotification.js
@@ -97,7 +97,42 @@ router.post('/', async (req, res) => {
     await sendMail(
       "oyerindei13@gmail.com",
       `Payment notification`,
-      `A payment was made. Here are the details:\n\n${JSON.stringify(customData, null, 2)}`
+      `<p>Your payment was successful. Here are the details of your invoice:</p>
+      <h2><b>Invoice Details</b></h2>
+      <table border="1" cellpadding="10" cellspacing="0" style="border-collapse: collapse; width: 100%;">
+        <tr>
+          <th style="text-align: left;">Field</th>
+          <th style="text-align: left;">Value</th>
+        </tr>
+        <tr>
+          <td><b>Name</b></td>
+          <td>${customData.customer_first_name} ${customData.customer_last_name}</td>
+        </tr>
+        <tr>
+          <td><b>Customer Email</b></td>
+          <td>${customData.customer_email}</td>
+        </tr>
+        <tr>
+          <td><b>Transaction ID</b></td>
+          <td>${customData.transaction_id}</td>
+        </tr>
+        <tr>
+          <td><b>Amount Paid</b></td>
+          <td>${customData.amount_paid}</td>
+        </tr>
+        <tr>
+          <td><b>Customer ID</b></td>
+          <td>${customData.customer_id}</td>
+        </tr>
+        <tr>
+          <td><b>Number</b></td>
+          <td>${customData.number}</td>
+        </tr>
+        <tr>
+          <td><b>Payment Status</b></td>
+          <td>${customData.payment_status}</td>
+        </tr>
+      </table>`
     );
 
     res.status(200).json({
